a6430706cae04e8c6ae2363864380aad4e5cd46a
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2024-06-05 12:10:25 -0300

    first commit

---
+++ src/main/java/TP4_CI/Tp4CiApplication.java
@@ -0,0 +1,13 @@
+package TP4_CI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4CiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4CiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TP4_CI/Tp4CiApplicationTests.java
@@ -0,0 +1,13 @@
+package TP4_CI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4CiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
