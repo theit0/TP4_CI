8f1423ac452a8532d92444fdfa69e5c84b242d05
pedrodonnarumma <pedrodonnarumma@gmail.com>
2024-06-05 16:37:22 -0300

    Tests Pedro hechos

--- src/test/java/TP4_CI/entitiesTests/ProveedorTests.java
+++ src/test/java/TP4_CI/entitiesTests/ProveedorTests.java
@@ -1,8 +1,14 @@
 package TP4_CI.entitiesTests;
 
+import TP4_CI.entities.Producto;
 import TP4_CI.entities.Proveedor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ProveedorTests {
@@ -10,8 +16,12 @@ public class ProveedorTests {
 
     @Test //Tercer test Pedro
     void testGetProductos(){
-        Proveedor proveedor = new Proveedor();
-
+        Producto producto = new Producto();
+        List<Producto> listaProductos = new ArrayList<>();
+        listaProductos.add(producto);
+        Proveedor proveedor = new Proveedor(1,listaProductos);
+        assertEquals(true,proveedor.getProductos().contains(producto));
     }
+
 
 }
